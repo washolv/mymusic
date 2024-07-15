1e411dea5f3c36723a6618d63c51866c4412c079
washolv <w_olv@hotmail.com>
2024-07-15 19:24:58 -0300

    first commit

---
+++ src/main/java/com/franciso/mymusic/MymusicApplication.java
@@ -0,0 +1,13 @@
+package com.franciso.mymusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franciso/mymusic/MymusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciso.mymusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
